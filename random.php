09f0e07ded1c235acdc64e9046f88353aea9a081
jyeric <jyeric1@126.com>
2019-09-19 15:51:18 +0800

    Update random.php

--- random.php
+++ random.php
@@ -5,11 +5,10 @@ $k = rand(0,count($str));
 $sina_img = str_re($str[$k]);
 $size_arr = array('large', 'mw1024', 'mw690', 'bmiddle', 'small', 'thumb180', 'thumbnail', 'square');
 $size = !empty($_GET['size']) ? $_GET['size'] : 'large' ;
-$server = rand(1,4);
 if(!in_array($size, $size_arr)){
     $size = 'large';
 }
-$url = 'https://ws'.$server.'.sinaimg.cn/'.$size.'/'.$sina_img.'.jpg';
+$url = 'https://raw.githubusercontent.com/jyeric/acg-pictures/master/1.jpg'.$size.'/'.$sina_img;
 //解析结果
 $result=array("code"=>"200","imgurl"=>"$url");
  
